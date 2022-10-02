1dce95fac9785409fba25488f70e36f0bf935c4d
gyubin <gbcompany27@gmail.com>
2022-10-02 16:18:11 +0900

    add mongodb connection

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const mongoose = require('mongoose');
+const env = require('dotenv');
+env.config();
 
 const app = express();
 app.use(express.json());
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const MONGO_URI = 'mongodb+srv://' + 
+                  process.env.MONGODB_USER + ':' + 
+                  process.env.MONGODB_PASSWORD + 
+                  '@madin.o0tnhkc.mongodb.net/?retryWrites=true&w=majority';
 
-const options = {
+const SWAGGER_OPTIONS = {
     definition: {
         openapi: '3.0.0',
         info: {
@@ -17,12 +24,21 @@ const options = {
     apis: ['./routes/*.js'],
 };
 
-const openapiSpec = swaggerJsdoc(options);
+const openapiSpec = swaggerJsdoc(SWAGGER_OPTIONS);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
+const server = async() => {
+    try {
+        let mongodbConnection = await mongoose.connect(MONGO_URI);
+        console.log('Mongo DB connected!');
+        
+        app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
+        app.use('/api',require('./routes/api'));
+        app.listen(3000, function() {
+            console.log('server listening on port 3000');
+        })
+    } catch(err) {
+        console.log(err);
+    }
+}
 
-app.use('/api',require('./routes/api'));
-
-app.listen(3000, function() {
-    console.log('server listening on port 3000');
-})
+server();
