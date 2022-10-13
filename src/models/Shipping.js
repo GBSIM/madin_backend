897df661b4bacb8f7fdc4d3fb14e9f2b4e38b3fd
Gyubin <gbcompany27@gmail.com>
2022-10-13 22:24:40 +0900

    add shipping schema

---
+++ src/models/Shipping.js
@@ -0,0 +1,15 @@
+const {Schema, model, Types:{ ObjectId }} = require('mongoose');
+const { ShippingSchema } = require('./Order');
+
+const ShippingSchema = new Schema({
+    name: {type: String, required: true},
+    phone: {type: Number, required: true},
+    address: {type: String, required: true},
+    request: {type: String, required: true, default: ""},
+    tag: {type: String, required: true, default: "배송지"},
+    user: {type: ObjectId, required: true, ref: "user"},
+}, {timestamps: true})
+
+const Shipping = model('shipping',ShippingSchema);
+
+module.exports = { Shipping, ShippingSchema };
