21b410ecdd025348ab47a53701c2014c6de3dcb4
gyubin <gbcompany27@gmail.com>
2022-10-24 17:23:10 +0900

    change user schema's username to name

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ const { OrderSchema } = require('./Order');
 const { ShippingSchema } = require('./Shipping');
 
 const UserSchema = new Schema({
-    username: { type: String, required: true},
+    name: { type: String, required: true},
     email: { type: String, required: true, unique: true},
     phone: { type: String, required: true, default: "01000000000"},
     profileImageUrl: String,
